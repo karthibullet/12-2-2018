83de768aacbd567f518baace41369af54d15edac
karthibullet <35765123+karthibullet@users.noreply.github.com>
2018-02-12 16:41:14 +0530

    41.c

--- 41.c
+++ 41.c
@@ -1,12 +1,15 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int i,k;
-    char s[100];
-    cin>>s>>k;
-	for(i=0;i<k;i++)
-	{
-	    cout<<s<<endl;
-	}
-	return 0;
+
+#include <stdio.h>
+
+int main()
+{
+    char s[50];
+    int i,n;
+    scanf("%s",s);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%s\n",s);
+    }
+    return 0;
 }
